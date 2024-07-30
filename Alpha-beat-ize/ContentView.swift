fabb8cb1b11c60338774d18af695c38c7adf7b3e
Andrew Jacob Paterson <Andrewjp97@me.com>
2024-07-30 19:05:11 -0400

    Working first prototype, allows user to enter guess, checks if correct, and proceeds through the alphabet to the end

--- Alpha-beat-ize/ContentView.swift
+++ Alpha-beat-ize/ContentView.swift
@@ -8,17 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var game: GameState?
+  @State private var currentWord: String = ""
+  @State private var isCorrect: Bool = false
+  var gameName: String
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      ZStack {
+        Rectangle()
+          .foregroundStyle(.accent)
+          .ignoresSafeArea()
+        ZStack {
+          Rectangle()
+            .foregroundStyle(.white)
+            .padding()
+          if !(game?.gameComplete ?? false) {
+            VStack {
+              HStack {
+                Text(game?.currentLetter.rawValue.uppercased(with: .autoupdatingCurrent) ?? "")
+                  .font(.system(size: 50))
+                  .padding(.leading, 40)
+                  .foregroundStyle(isCorrect ? .accent : .black)
+                TextField("Word", text: $currentWord)
+                  .font(.system(size: 50))
+                  .autocorrectionDisabled()
+                  .textInputAutocapitalization(.never)
+                  .offset(x: -10)
+                  .foregroundStyle(isCorrect ? .accent : .black)
+                  .disabled(isCorrect)
+                  .onSubmit {
+                    if let game = game {
+                      isCorrect = game.checkAnswer(answer: game.currentLetter.rawValue + currentWord)
+                      print(isCorrect)
+                    }
+                  }
+              }
+              if isCorrect {
+                Button("Next") {
+                  if game != nil {
+                    game!.advance()
+                    currentWord = ""
+                    isCorrect = false
+                  }
+                }
+              }
+              else {
+                Button("Skip") {
+                  if game != nil {
+                    game!.advance()
+                    currentWord = ""
+                    isCorrect = false
+                  }
+                }
+              }
+            }
+          }
+          else {
+            VStack{
+              Text("You win!")
+                .font(.system(size: 50))
+                .foregroundStyle(.accent)
+              Button("Play again") {
+                game!.gameComplete = false
+                currentWord = ""
+                isCorrect = false
+              }
+            }
+          }
         }
-        .padding()
+      }
+        .task {
+          let items = loadItems(gameName)
+          game = GameState(currentLetter: .a, letterDictionary: items)
+          print(game!)
+        }
     }
+  
+  func loadItems(_ name: String) -> ItemDictionary {
+    let asset = NSDataAsset(name: name)
+    return try! JSONDecoder().decode(ItemDictionary.self, from: asset!.data)
+  }
+  
 }
 
 #Preview {
-    ContentView()
+    ContentView(gameName: "fruits")
 }
