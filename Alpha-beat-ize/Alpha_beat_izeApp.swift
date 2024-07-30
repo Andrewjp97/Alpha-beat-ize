6e7696fc873ea3ca2f2a8633308e3aa5700fc589
Andrew Jacob Paterson <Andrewjp97@me.com>
2024-07-30 08:34:04 -0400

    Initial Commit

---
+++ Alpha-beat-ize/Alpha_beat_izeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alpha_beat_izeApp.swift
+//  Alpha-beat-ize
+//
+//  Created by Andrew Paterson on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Alpha_beat_izeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
